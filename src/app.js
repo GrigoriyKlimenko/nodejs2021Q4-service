e76214bd53bd87607e4606ceb06cfbf20c7b5294
grigoriy_klimenko <gklimenko>
2021-12-04 11:35:26 +0300

    add swagger docs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const fastify = require('fastify');
 const usersRouter = require('./resources/users/user.router');
 
 const app = fastify({logger:true});
+app.register(require('fastify-swagger'), {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: { title: 'fastify-service' },
+    }
+})
 app.register(usersRouter);
 
 module.exports = app;
