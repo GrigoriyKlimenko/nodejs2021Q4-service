1176cc1640eb5b00178f3c7fee48d97cdcb64957
grigoriy_klimenko <gklimenko>
2021-12-25 15:25:49 +0300

    add writing to file

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,5 +1,20 @@
-import pino from 'pino';
+import { TransportMultiOptions, pino, Logger } from "pino";
 
-const logger = pino();
+const transport = pino.transport(<TransportMultiOptions>{
+    targets: [
+        {
+            target: 'pino/file',
+            level: 'error',
+            options: {destination: './logs/error.txt', mkdir: true}
+        },
+        {
+            target: 'pino/file',
+            level: 'trace',
+            options: {destination: './logs/all.txt', mkdir: true}
+        }
+    ]
+});
+
+const logger: Logger = pino(transport);
 
 export default logger;
