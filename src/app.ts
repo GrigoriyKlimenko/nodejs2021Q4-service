f542dd6fad9ab299b5a82e9245b00cb032892683
grigoriy_klimenko <gklimenko>
2021-12-25 18:13:07 +0300

    add logging for body, params and query

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,18 @@ import tasksRouter from './resources/tasks/tasks.router';
 
 
 const app = fastify({logger: logger});
-
+app.addHook('preHandler', function (req, _res, done) {
+    if (req.body) {
+      req.log.info({ body: req.body }, 'parsed body')
+    }
+    if (req.params) {
+        req.log.info({ params: req.params }, 'params')
+    }
+    if (req.query) {
+        req.log.info({ query: req.query }, 'query')
+    }
+    done()
+  })
 app.register(usersRouter);
 app.register(boardsRouter);
 app.register(tasksRouter);
