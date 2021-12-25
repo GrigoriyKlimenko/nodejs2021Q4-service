fd05b7e0251b90c6f90be523170ba015401ee426
grigoriy_klimenko <gklimenko>
2021-12-25 15:54:24 +0300

    add pino pretty

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -5,12 +5,19 @@ const transport = pino.transport(<TransportMultiOptions>{
         {
             target: 'pino/file',
             level: 'error',
-            options: {destination: './logs/error.txt', mkdir: true}
+            options: {destination: './logs/error.txt', mkdir: true},
         },
         {
             target: 'pino/file',
             level: 'trace',
-            options: {destination: './logs/all.txt', mkdir: true}
+            options: {destination: './logs/all.txt', mkdir: true},
+        },
+        {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              translateTime: true,
+            }
         }
     ]
 });
